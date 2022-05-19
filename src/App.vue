afbed139d270c4b0dcd29d887a4f19140ace3a01
Bombay <OMGmsebastian@gmail.com>
2022-05-19 02:25:55 -0400

    t

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Intro from './components/Intro.vue'
+</script>
+
+<template>
+  <Navbar />
+  <Intro />
+</template>
+
+<style>
+#app {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+}
+</style>
